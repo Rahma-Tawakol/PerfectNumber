8cd80f5d3355427bb05ccd34a70cb95d2208ed61
Rahma-Tawakol <121264633+Rahma-Tawakol@users.noreply.github.com>
2022-12-23 21:01:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Perfectnum
+{
+    public static class Program
+    {
+        public static int FindFactors(int n2)
+        {
+            int sum = 0;
+            for (int i = 1; i < n2; i++)
+            {
+                if (n2 % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("please enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n2; i++)
+            {
+                int result = FindFactors(i);
+                if (i > n1)
+                {
+                    if (i == result)
+                    {
+                        Console.Write("\t" + i);
+                    }
+                }
+            }
+        }
+    }
+}
